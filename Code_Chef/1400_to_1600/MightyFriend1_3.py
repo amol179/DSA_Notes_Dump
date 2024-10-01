2a87f1e53303cfc4d97117cbd4e505ffbd7d8ad8
Amol Gurbhele <amol179>
2024-10-01 15:47:48 +0530

    updates

--- Code_Chef/1400_to_1600/MightyFriend1_3.py
+++ Code_Chef/1400_to_1600/MightyFriend1_3.py
@@ -1,25 +1,24 @@
-for _ in range(int(input())):
+t = int(input())
+for _ in range(t):
+    if t >= 1 and t <= 100:
+        N, K = map(int, input().split())
 
-    N, K = map(int, input().split())
+    if N >= 1 and N <= 10 ^ 5 and K >= 0 and K <= 10 ^ 5:
+        A = list(map(int, input().split()))
 
-    M_score = 0
-    T_score = 0
+        M = sorted(A[0:N:2], reverse=True)
+        T = sorted(A[1:N:2])
 
-    A = list(map(int, input().split()))
+        for i in range(min(K, len(M), len(T))):
+            if T[i] < M[i]:
+                M[i], T[i] = T[i], M[i]
+            else:
+                break
 
-    M = sorted(A[0:N:2], reverse=True)
-    T = sorted(A[1:N:2])
+        M_score = sum(M)
+        T_score = sum(T)
 
-    for i in range(min(K, len(M), len(T))):
-        if T[i] < M[i]:
-            M[i], T[i] = T[i], M[i]
+        if T_score > M_score:
+            print("Yes")
         else:
-            break
-
-    M_score = sum(M)
-    T_score = sum(T)
-
-    if T_score > M_score:
-        print("Yes")
-    else:
-        print("NO")
+            print("NO")
