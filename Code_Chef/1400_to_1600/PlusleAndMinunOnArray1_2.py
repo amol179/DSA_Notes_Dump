0d5a1354dff465e915c0b98941d5cd61cf371f12
Amol Gurbhele <amol179>
2024-10-01 18:46:08 +0530

    updates

--- Code_Chef/1400_to_1600/PlusleAndMinunOnArray1_2.py
+++ Code_Chef/1400_to_1600/PlusleAndMinunOnArray1_2.py
@@ -1,7 +1,7 @@
 for _ in range(int(input())):
     N = int(input())
     A = [int(x) for x in input().split()]
-    for i in range(n):
+    for i in range(N):
         A[i] = abs(A[i])
     add = sorted(A[::2])
     minus = sorted(A[1::2], reverse=True)
