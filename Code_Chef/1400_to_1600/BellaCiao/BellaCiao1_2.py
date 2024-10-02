f5315c287fe0c1916310efa19bc495b0696ad7bc
Amol Gurbhele <amol179>
2024-10-02 21:36:53 +0530

    updates

--- Code_Chef/1400_to_1600/BellaCiao/BellaCiao1_2.py
+++ Code_Chef/1400_to_1600/BellaCiao/BellaCiao1_2.py
@@ -2,9 +2,9 @@ T = int(input())
 for _ in range(T):
     D, d, P, Q = map(int, input().split())
 
-    interval = D // d  # interval of change
-    remDay = D % d
-    change_ofRate = P + Q * interval
+    interval = D // d  # complete interval of change
+    remDay = D % d  # remaing day, incomplete cycle
+    change_ofRate = P + Q * interval  # production rate
 
     production = d * (P * interval + Q * ((interval - 1) * interval) // 2)
     remDay_production = remDay * change_ofRate
